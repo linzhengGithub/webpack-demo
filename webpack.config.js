d697e10551621637fb3f0fb15b040b59f05323e8
linzheng <947169094@qq.com>
2020-06-20 00:45:35 +0800

    three

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
 
 module.exports = {
   mode: "development",
@@ -16,12 +17,26 @@ module.exports = {
       title: "林铮",
       template: "src/assets/index.html",
     }),
+    new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[id].[contenthash].css',
+      }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: '../',
+                hmr: process.env.NODE_ENV === 'development',
+              },
+            },
+            'css-loader',
+          ],
+        // use: ["style-loader", "css-loader"],
       },
     ],
   },
