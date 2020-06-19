c04bf850cc0427b6a1e7e293c07f4f79dcb5c7aa
linzheng <947169094@qq.com>
2020-06-20 00:39:05 +0800

    two

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[contenthash].js',
+  mode: "development",
+  devServer: {
+    contentBase: "./dist",
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: '林铮',
-    template: 'src/assets/index.html'
-  })],
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "林铮",
+      template: "src/assets/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
