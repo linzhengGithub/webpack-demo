c0d5b61f8f758b4807732ae3b4175a52caad2168
linzheng <947169094@qq.com>
2020-06-20 00:34:42 +0800

    one

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[contenthash].js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '林铮',
+    template: 'src/assets/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
