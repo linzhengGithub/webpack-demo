a16b65626efe8b2914d1123dc46d438a589900e9
linzheng <947169094@qq.com>
2020-06-20 16:17:30 +0800

    four

--- webpack.config.js
+++ webpack.config.js
@@ -2,41 +2,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require("path");
 
+const base = require('./webpack.config.base.js')
+
 module.exports = {
+  ...base,
   mode: "development",
   devServer: {
     contentBase: "./dist",
   },
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.[contenthash].js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "林铮",
-      template: "src/assets/index.html",
-    }),
-    new MiniCssExtractPlugin({
-        filename: '[name].[contenthash].css',
-        chunkFilename: '[id].[contenthash].css',
-      }),
-  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                publicPath: '../',
-                hmr: process.env.NODE_ENV === 'development',
-              },
-            },
-            'css-loader',
-          ],
-        // use: ["style-loader", "css-loader"],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
