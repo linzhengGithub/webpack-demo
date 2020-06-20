9f1197295f87a014882a2db271ecd532575e18fb
linzheng <947169094@qq.com>
2020-06-20 20:17:39 +0800

    ready

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import x from './x.js'
+import jpg from './assets/1.jpg'
+console.log(jpg)
+
+const div = document.getElementById('app')
+div.innerHTML = `<img src="${jpg}">`
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+div.appendChild(button)
+button.onclick = () => {
+    const promise = import('./lazy.js')
+    promise.then((module)=>{
+        const fn = module.default
+        fn()
+    },()=>{
+        console.log('模块加载错误')
+    })
+}
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,42 @@ module.exports = {
       title: "林铮",
       template: "src/assets/index.html",
     }),
-  ]
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.styl$/,
+        loader: [
+          "style-loader",
+          "css-loader",
+          "stylus-loader",
+        ],
+      },
+      {
+        test: /\.less$/,
+        loader: [
+          "style-loader",
+          "css-loader",
+          "less-loader",
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
